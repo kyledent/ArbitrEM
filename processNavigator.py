fda2127d2c5510ec2d7f0447ada5666fc1ed3b8d
Kyle Dent <kyle.dent@gmail.com>
2019-09-01 18:17:14 +0100

    processNavigator.py now embedds settings into the runArbitrEM script for ease of use

--- processNavigator.py
+++ processNavigator.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# A script by Kyle Dent for use with ArbitrEM SerialEM script. Updated 27 July 2019
+# A script by Kyle Dent for use with ArbitrEM SerialEM script. Updated 1 Sept 2019
 # When running the script on a navigator file, please make sure that all polygons, medium magnification 
 # montages and acquisition template points have been removed.
 # Please make sure that the first item in the navigator is your Atlas/Specimen Grid Montage.
@@ -12,47 +12,57 @@
 
 from __future__ import division
 
-import math, re, sys, argparse, os, sys
+import math, re, sys, argparse, os, sys, datetime
+
+from string import Template
 
 parser = argparse.ArgumentParser(description='ArbitrEM Python script to process a SerialEM navigator allowing acquisition points to be associated with specific view-maps. ArbitrEM session files are output by default into ./arbitrEM.')
 parser.add_argument('--o', default='./arbitrEM', help='output directory for ArbitrEM files')
 parser.add_argument('--d', default=None, type=float, help='the diameter of the targeting view-map in micrometers')
 parser.add_argument('--nav', default=None, help='the SerialEM navigator file (not in XML format)')
-parser.add_argument('--v', default='False',help='Request verbose output for diagnositic purposes')
-parser.add_argument('--s', default='True',help='set to "True" to request verbose output for diagnositic purposes')
+parser.add_argument('--v', default='False', action='store_true',help='request verbose output for diagnositic purposes')
+parser.add_argument('--s', default='True', action='store_true',help='request creation of settings files as well as SerialEM scripts with settings embedded')
 parser.add_argument('--generateSettings', default='True',help='whether or not to generate the settings')
 parser.add_argument('--customShiftOffset', default='0.0 0.0',help='apply a custom beam-image shift offset to systematically adjust calculated targeting X,Y shifts.')
-parser.add_argument('--defocusRange', default='-1 -2.6 0.2',help='the defocus range and step, specify for example as \'-1 -2.5 0.2\'. Step must be positive.)
+parser.add_argument('--defocusRange', default='-1 -2.6 0.2',help='the defocus range and step, specify for example as \'-1 -2.5 0.2\'. Step must be positive.')
 parser.add_argument('--earlyReturn', default='1',help=' 0 - early return ON, 1 - early return OFF i.e. SerialEM parameter values')
-parser.add_argument('--scriptTemplate', default='./script_templates/ArbitrEM.txt', help='location of template script into which parameters will be imbeded')
-parser.add_argument('--sessionBasePath', default=None, help='session base path on the SerialEM computer. e.g.: D:\session\specimen_X\')
+parser.add_argument('--scriptTemplate', default='./ArbitrEM.txt', help='location of template script into which parameters will be embedded')
+parser.add_argument('--sessionBasePath', default=None, type=str, help="session base path on the SerialEM computer. e.g.: D:\session\specimen_X'")
+parser.add_argument('--viewMapDefocus', default='-75',help='The defocus used for view-map acquisition.')
+parser.add_argument('--viewMapExpTime', default='1',help='The exposure time used for view-map acquisition.')
 
 args_dict = vars(parser.parse_args())
 
 if None in args_dict.values():
     if args_dict['d'] is None:
-        print('Please specify the diameter of your view-maps and try again. Run ./processNavigator.py --help for more details.')
+        print('Please specify the diameter (in microns) of your view-maps and try again. Run ./processNavigator.py --help for more details.')
     elif args_dict['nav'] is None:
         print('Please specify a navigator file and try again. The format should be SerialEM and not XML. Run ./processNavigator.py --help for more details.')  
-    elif args_dict['sessionBasePath']
+    elif args_dict['sessionBasePath'] is None:
+        print('Please specify a session base path on the computer running SerialEM in quotation marks, e.g. --sessionBasePath "D:\session\specimen_X". The outputed sessions files must be placed in side this folder, e.g. D:\session\specimen_X\arbitrEM')
+    parser.print_help()
     quit()
-    
-try:
-    args_dict['v'] = eval(args_dict['v'])
-    args_dict['s'] = eval(args_dict['s'])
 
-except Exception as e:
-    print('Problem interpreting boolean variable. Please check you spelling.')
+#try:
+#    args_dict['v'] = eval(args_dict['v'])
+#    args_dict['s'] = eval(args_dict['s'])
+#
+#except Exception as e:
+#    print('Problem interpreting boolean variable. Please check you spelling.')
 
 settingsFiles = {'customShift.txt':args_dict['customShiftOffset'],
                  'defocusRange.txt':args_dict['defocusRange'],
                  'earlyReturn.txt':args_dict['earlyReturn']}
+                    
+settingsDict = {'viewMapDiameter':args_dict['d'], 'sessionBasePath':args_dict['sessionBasePath'],
+               'viewMapDefocus': args_dict['viewMapDefocus'], 'viewMapExpTime': args_dict['viewMapExpTime']}
 
 # Navigator items have the format: [Item = LABEL]
 outputDir = args_dict['o']
 
 if os.path.isdir(outputDir) == False:
     os.makedirs(outputDir)
+    
 print('ArbitrEM related files are being written to: {}'.format(outputDir))
 
 foilHoleDiameter = float(args_dict['d'])
@@ -78,11 +88,18 @@ def createSettingsFiles():
         except Exception as e:
             print(str(e))
 
-def readFileIntoStringTemplate(inputFile):
-    with open(inputFile,'r') as fileHandle: #slurm_skubmit_peta4-skylake_template
+def writeArbitrEMScript(inputFile,settingsDict,outputFile):
+    with open(inputFile,'r') as fileHandle:
         fileContents=''.join(fileHandle.readlines())
-        fileStringTemplate = Template(str(fileContents))
-        return fileStringTemplate
+        stringTemplate = Template(str(fileContents))
+        fileHandle.close()
+    
+    print('Creating {} script with {} specified as the SerialEM session base-path'.format(outputFile,args_dict['sessionBasePath']))
+    submitScriptString = stringTemplate.safe_substitute(settingsDict)
+    
+    scriptHandle=open(outputFile,'w')
+    scriptHandle.write(submitScriptString)
+    scriptHandle.close()
 
 def calculateDistance(anchor_item, point_item):
     distance_calc = 0
@@ -189,5 +206,7 @@ print("Processed navigigator file {}.\n{} hole/area maps are set to acquire, and
 
 if args_dict['s']:
     createSettingsFiles()
-
+    scriptOutputPath=os.path.join(args_dict['o'],'runArbitrEM.txt')
+    writeArbitrEMScript(args_dict['scriptTemplate'], settingsDict, scriptOutputPath)  
+             
 print('Finished.')
